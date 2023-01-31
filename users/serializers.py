d87653e1688599f261b911b837d9ace032129ab1
larissa-sato <larissasato_@outlook.com>
2023-01-31 09:54:40 -0300

    saved users serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
-from user.models import User
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+        read_only_fields = ['is_superuser']
         extra_kwargs = {'password': {'write_only':True}}
 
         def create(self, validated_data: dict) -> User:
